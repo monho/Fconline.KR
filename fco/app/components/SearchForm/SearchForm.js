a9d178124bf410e0a33379d25ca355d8bd5fd9b2
MoonHo <75512752+monho@users.noreply.github.com>
2024-01-12 16:39:20 +0900

    Update SearchForm.js

--- fco/app/components/SearchForm/SearchForm.js
+++ fco/app/components/SearchForm/SearchForm.js
@@ -1,5 +1,5 @@
 'use client'
-import { useRouter,usePathname } from next/navigation;
+import { usePathname } from next/navigation;
 import { useEffect, useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 
